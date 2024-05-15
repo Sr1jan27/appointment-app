62f593f8c43961845e595337c159f8de897d038f
srijan_singh <singhsrijan809@gmail.com>
2024-05-15 10:17:32 +0530

    used ngOnInit for loading the data present in local storage

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
+ 
 
   appointment: Appointment = {
     id: 1,
@@ -16,6 +18,12 @@ export class AppointmentListComponent {
   newAppointmentTitle: string = "";
   newAppointmentDate: Date = new Date();
   appointments: Appointment[] = [];
+
+  ngOnInit(): void {
+    let savedAppoinments = localStorage.getItem("appointments");
+
+    this.appointments = savedAppoinments? JSON.parse(savedAppoinments) : [];
+  }
 
   addApointment(){
 
